fb80e6cbca1db0073d23418b7f9eef5d7b32c7f7
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-02-09 21:38:49 -0800

    Write recursive-based function for fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,9 +1,17 @@
-function fibs(position) {
+function fibs(num) {
   let arr = [0,1];
-  for (let i = 2; i < position; i++) {
+  for (let i = 2; i < num; i++) {
     arr[i] = arr[i - 2] + arr[i - 1];
   }
   return arr;
 }
 
-console.log(fibs(8));
+function fibsRec(num, arr = [1,0], i = 2) {
+  if (num === 1) return [0];
+  if (i === num) return arr.reverse();
+
+  arr.unshift(arr[0] + arr[1]);
+  return fibsRec(num, arr, i + 1);
+}
+
+console.log(fibsRec(8));
