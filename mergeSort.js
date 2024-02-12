91b41adbdfb347570b75b37e3476c8669a0b7a56
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-02-11 23:36:48 -0800

    Complete merge functionality of mergeSort()

--- mergeSort.js
+++ mergeSort.js
@@ -2,15 +2,43 @@ function mergeSort(arr) {
   if (arr.length === 1) {
     return arr;
   }
+
   let midpointIndex = Math.floor((arr.length) / 2);
   let leftArr = arr.slice(0,midpointIndex);
   let rightArr = arr.slice(midpointIndex);
-  console.log(leftArr, rightArr);
+  
   return [
-    ...mergeSort(leftArr),
-    ...mergeSort(rightArr)
+    ...merge(
+    mergeSort(leftArr),
+    mergeSort(rightArr)
+    )
   ];
 }
 
-console.log(mergeSort([3,1,2,7,0]));
+function merge(leftArr, rightArr, leftIndex = 0, rightIndex = 0) {
+  const leftArrLength = leftArr.length;
+  const rightArrLength = rightArr.length;
+  let sortedArr = [];
+
+  // loop until one of the arrays completes
+  while (leftIndex < leftArrLength
+    && rightIndex < rightArrLength) {
+    if (leftArr[leftIndex] < rightArr[rightIndex]) {
+      sortedArr.push(leftArr[leftIndex++]);
+    } else {
+      sortedArr.push(rightArr[rightIndex++]);
+    }
+  }
+
+  // append the remaining elements from the incomplete array
+  if (leftIndex === leftArrLength) {
+    sortedArr = [...sortedArr, ...rightArr.slice(rightIndex)];
+  } else {
+    sortedArr = [...sortedArr, ...leftArr.slice(leftIndex)];
+  }
+
+  return sortedArr;
+}
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
 
